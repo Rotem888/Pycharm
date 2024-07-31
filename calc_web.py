13d7000197ace6c7d580651515234451d117bb02
Rotem <you@example.com>
2024-07-31 12:29:07 +0300

    initial commit

---
+++ calc_web.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+# creates a Flask application
+app = Flask(__name__, template_folder='templates/')
+
+@app.route("/")
+def tipcalc_web():
+    return render_template('index.html')
+
+# run the application
+app.run(debug=True)
